a9fdd07c5777eb3932f2f8efc6565b8d7df9b215
roberttangwu <55282102+roberttangwu@users.noreply.github.com>
2020-01-10 15:31:55 +0000

    Add files via upload

---
+++ Modular.calculator/Modular.calculator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular.calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalc = new Calculator();
+
+            Console.WriteLine("##################################################");
+            Console.WriteLine("############# C# C A L C U L A T O R #############");
+            Console.WriteLine("##################################################");
+            bool calcLoop = true;
+            while (calcLoop == true)
+            {
+                Console.Write("Enter your first number: ");
+                double inputOne = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter your second number: ");
+                double inputTwo = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("Enter 1 for addition");
+                Console.WriteLine("Enter 2 for subtraction");
+                Console.WriteLine("Enter 3 for multiplication");
+                Console.WriteLine("Enter 4 for division");
+                Console.WriteLine("");
+                int opChoice = Convert.ToInt32(Console.ReadLine());
+                switch (opChoice)
+                {
+                    case 1:
+                        Console.WriteLine("\nYour answer is: " + myCalc.Add(inputOne, inputTwo).ToString());
+                        break;
+                    case 2:
+                        Console.WriteLine("\nYour answer is: " + myCalc.Subtract(inputOne, inputTwo).ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine("\nYour answer is: " + myCalc.Multiply(inputOne, inputTwo).ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine("\nYour answer is: " + myCalc.Divide(inputOne, inputTwo).ToString());
+                        break;
+                    default:
+                        Console.WriteLine("\nYou should enter a number from the menu. ");
+                        break;
+
+
+                }
+                Console.WriteLine("");
+                Console.Write("\nDo you want to try another sum? Y/N: ");
+                string restartChoice = Console.ReadLine().ToUpper();
+
+                if (restartChoice == "Y")
+                {
+                    Console.WriteLine("press any key to continue");
+                    Console.ReadKey();
+                
+                }
+                else
+                {
+                    Console.WriteLine("press any key to exit");
+                    calcLoop = false;
+
+                }
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Modular.calculator/Modular.calculator/calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modular.calculator
+{
+    public class Calculator
+
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            if(num1 > num2)
+            {
+                return num1 - num2;
+            }
+            return num2 - num1;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
